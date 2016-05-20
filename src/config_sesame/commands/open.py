7896a0c3bdf1ec96cc61896e07711929dde2a01a
jhermann <jh@web.de>
2016-05-20 16:58:16 +0200

    open: load simple-merged YAML trees, and dump the result for now

--- src/config_sesame/commands/open.py
+++ src/config_sesame/commands/open.py
@@ -17,16 +17,44 @@
 # limitations under the License.
 from __future__ import absolute_import, unicode_literals, print_function
 
+import io
 import os
 import sys
 
+import yaml
+from pyaml import dump as ppyaml
+from click.exceptions import UsageError
 from rudiments.reamed import click
 
 from .. import config
 
 
+def load_all(filename):
+    """Generate objects contained in ``filename``."""
+    if filename.endswith('.yaml') or filename.endswith('.yml'):
+        with io.open(filename, encoding='utf-8') as handle:
+            for obj in yaml.load_all(handle):
+                yield obj
+    else:
+        raise UsageError("Unsupported file type (extension) in '{}'!".format(filename))
+
+
+def read_merged_files(cfgfiles):
+    """Read a list of hierachical config files, and merge their keys."""
+    result = {}
+    for cfgfile in cfgfiles:
+        for obj in load_all(cfgfile):
+            result.update(obj) # TODO: sensible merging strategy
+    return result
+
+
 @config.cli.command(name='open')
+@click.argument('cfgfile', nargs=-1)
 @click.pass_context
-def open_command(ctx):
-    """Open vault and amend configuration file."""
-    print("Not implemented!")
+def open_command(ctx, cfgfile=None):
+    """Open vault and amend configuration file(s)."""
+    if not cfgfile:
+        raise UsageError("You provided no configuration file names!", ctx=ctx)
+
+    cfgdata = read_merged_files(cfgfile)
+    ppyaml(cfgdata, sys.stdout)
